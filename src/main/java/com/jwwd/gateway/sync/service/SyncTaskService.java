30635bdff3636c6c8548b0b2b9daea508398a4e8
Jhon Swiffet <143003037+Zizkl@users.noreply.github.com>
2026-04-24 17:06:51 +0800

    Implement alarm trigger checks

--- src/main/java/com/jwwd/gateway/sync/service/SyncTaskService.java
+++ src/main/java/com/jwwd/gateway/sync/service/SyncTaskService.java
@@ -2,6 +2,9 @@ package com.jwwd.gateway.sync.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.jwwd.gateway.sync.entity.SyncTask;
+import com.jwwd.gateway.sync.enums.SyncTaskType;
 
 public interface SyncTaskService extends IService<SyncTask> {
+
+    SyncTask createPendingTask(SyncTaskType taskType, Long businessId, Object payload);
 }
