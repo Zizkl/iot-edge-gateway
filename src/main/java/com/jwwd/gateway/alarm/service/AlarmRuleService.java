99c63e93f83c33ac772679aff757d85de9c2ec58
Jhon Swiffet <143003037+Zizkl@users.noreply.github.com>
2026-04-24 17:05:48 +0800

    Implement alarm trigger checks

--- src/main/java/com/jwwd/gateway/alarm/service/AlarmRuleService.java
+++ src/main/java/com/jwwd/gateway/alarm/service/AlarmRuleService.java
@@ -2,6 +2,11 @@ package com.jwwd.gateway.alarm.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.jwwd.gateway.alarm.entity.AlarmRule;
+import com.jwwd.gateway.metric.entity.DeviceMetricData;
+
+import java.util.List;
 
 public interface AlarmRuleService extends IService<AlarmRule> {
+
+    List<AlarmRule> listEnabledRules(DeviceMetricData metricData);
 }
